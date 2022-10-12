b0e7f75eee1969fb437e03eab00d08c0e4a08307
Jose Martinez <44812411+JoseM111@users.noreply.github.com>
2022-10-12 16:14:01 -0400

    EPIC0001: added more type safety & reactivity [ OCT 12th ]
    Took 25 minutes

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,12 +1,34 @@
 <!-- @TimeLine -->
 <script setup lang="ts">
-const periods = ['Today', 'This Week', 'This Month'];
+import { ref } from 'vue';
+
+// as const: makes the array read only
+const periods: readonly [string, string, string] = [
+    'Today',
+    'This Week',
+    'This Month',
+] as const;
+
+type PeriodType = typeof periods[number];
+
+const selectedPeriod = ref<PeriodType>('Today');
+
+const selectPeriod = (period: PeriodType) => {
+    selectedPeriod.value = period;
+};
 </script>
 
 <template>
     <nav class="is-primary panel">
         <span class="panel-tabs">
-            <a v-for="period of periods" :key="period">
+            <a
+                v-for="period of periods"
+                :key="period"
+                :class="{
+                    'is-active': period === selectedPeriod,
+                }"
+                @click="selectPeriod(period)"
+            >
                 {{ period }}
             </a>
         </span>
