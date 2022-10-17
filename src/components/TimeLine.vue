642d2a3774ba12a897d4795309c44544c1d2d23c
Jose Martinez <44812411+JoseM111@users.noreply.github.com>
2022-10-17 07:33:10 -0400

    EPIC0002: refactored the TimeLine component [ OCT 17th ]
    Took 23 minutes

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,58 +1,10 @@
 <!-- @TimeLine -->
 <script setup lang="ts">
 import TimeLineItem from '@/components/TimeLineItem.vue';
-import {
-  thisMonth,
-  thisWeek,
-  TimeLinePostType,
-  today,
-} from '@/dummy_data/post';
+import { periods } from '@/stores/constants';
 import { usePostStore } from '@/stores/postStore';
-import { DateTime } from 'luxon';
-import { computed, ref } from 'vue';
 
 const postStore = usePostStore();
-// as const: makes the array read only
-const periods: readonly [string, string, string] = [
-  'Today',
-  'This Week',
-  'This Month',
-] as const;
-
-type PeriodType = typeof periods[number];
-
-const selectedPeriod = ref<PeriodType>('Today');
-
-// everytime our selectedPeriod changes, computed will cause a re-render
-const postList = computed<Array<TimeLinePostType>>(() => {
-  return postStore.ids
-    .map((id) => {
-      const post = postStore.all.get(id);
-
-      if (post) {
-        return {
-          ...post,
-          created: DateTime.fromISO(post.created),
-        };
-      }
-
-      throw Error(`Post with id ${id} not found`);
-    })
-    .filter((post) => {
-      switch (selectedPeriod.value) {
-        case 'Today':
-          return post.created >= DateTime.now().minus({ day: 1 });
-        case 'This Week':
-          return post.created >= DateTime.now().minus({ week: 1 });
-      }
-
-      return post;
-    });
-});
-
-const selectPeriod = (period: PeriodType) => {
-  selectedPeriod.value = period;
-};
 </script>
 
 <template>
@@ -61,8 +13,8 @@ const selectPeriod = (period: PeriodType) => {
       <a
         v-for="period of periods"
         :key="period"
-        :class="{ 'is-active': period === selectedPeriod }"
-        @click="selectPeriod(period)"
+        :class="{ 'is-active': period === postStore.selectedPeriod }"
+        @click="postStore.setSelectedPeriod(period)"
       >
         {{ period }}
       </a>
@@ -70,7 +22,7 @@ const selectPeriod = (period: PeriodType) => {
 
     <!-- LIST OF POST -->
     <TimeLineItem
-      v-for="post of postList"
+      v-for="post of postStore.filteredPosts"
       :key="post.id"
       :post="post"
     />
--- src/stores/postStore.ts
+++ src/stores/postStore.ts
@@ -1,10 +1,19 @@
-// postStore.ts
-import { IPost, thisMonth, thisWeek, today } from '@/dummy_data/post';
+// state.ts
+import {
+  IPost,
+  thisMonth,
+  thisWeek,
+  TimeLinePostType,
+  today,
+} from '@/dummy_data/post';
+import { PeriodType } from '@/stores/constants';
+import { DateTime } from 'luxon';
 import { defineStore } from 'pinia';
 
 interface IPostState {
   ids: Array<string>;
   all: Map<string, IPost>;
+  selectedPeriod: PeriodType;
 }
 
 export const usePostStore = defineStore('posts', {
@@ -16,8 +25,44 @@ export const usePostStore = defineStore('posts', {
       [thisWeek.id, thisWeek],
       [thisMonth.id, thisMonth],
     ]),
+    selectedPeriod: 'Today',
   }),
   // actions: to update the state
-  actions: {},
-  getters: {},
+  actions: {
+    setSelectedPeriod(period: PeriodType) {
+      this.selectedPeriod = period;
+    },
+  },
+  // works similarly to a computed property.
+  // So a new state based on an existing state
+  getters: {
+    filteredPosts: (state): Array<TimeLinePostType> => {
+      // mapping through the `ids` Array<string, IPost>
+      return state.ids
+        .map((id) => {
+          const post = state.all.get(id);
+
+          if (post) {
+            return {
+              ...post,
+              created: DateTime.fromISO(post.created),
+            };
+          }
+
+          throw Error(`Post with id ${id} not found`);
+        })
+        .filter((post) => {
+          switch (state.selectedPeriod) {
+            case 'Today':
+              return post.created >= DateTime.now().minus({ day: 1 });
+            case 'This Week':
+              return (
+                post.created >= DateTime.now().minus({ week: 1 })
+              );
+          }
+
+          return post;
+        });
+    },
+  },
 });
