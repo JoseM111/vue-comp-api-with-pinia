b5774432b668acc8d9dae7d745db3d4a5960610c
Jose Martinez <44812411+JoseM111@users.noreply.github.com>
2022-10-18 07:34:33 -0400

    EPIC0003: created a PostWriter component that ccesses dom-nodes [ OCT 18th ]
    Took 17 minutes

--- src/components/PostWriter.vue
+++ src/components/PostWriter.vue
@@ -1,7 +1,7 @@
 <!-- @PostWriter -->
 <script lang="ts" setup>
 import { TimeLinePostType } from '@/dummy_data/posts';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 const props = defineProps<{
   post: TimeLinePostType;
@@ -9,6 +9,13 @@ const props = defineProps<{
 
 const { post } = props;
 const title = ref<string>(post.title);
+// will be call to access the dom node: div
+const contentEditable = ref<HTMLDivElement>();
+
+// functions
+onMounted(() => {
+  console.log('on_mount_contentEditable:\n', contentEditable.value?.innerText);
+});
 </script>
 <template>
   <!-- COLUMNS -->
@@ -19,9 +26,26 @@ const title = ref<string>(post.title);
       <div class="field">
         <!-- LABEL -->
         <div class="label">Post Title</div>
+
         <!-- INPUT (where we will write the title to our post) -->
         <input type="text" class="input" v-model="title" />
         {{ title }}
+      </div>
+
+      <!--
+        The contenteditable global attribute  is an enumerated attribute
+        indicating if the element should be editable by the user. If so,
+        the browser modifies its widget to allow editing. The attribute
+        must take one of the following values:
+        true or an empty string, which indicates that the element is editable.
+        false, which indicates that the element is not editable.
+        If the attribute is given without a value, like
+        <label contenteditable>Example Label</label>, its value is treated
+        as an empty string.
+        (adds a reactive reference to the dom-node)
+       -->
+      <div contenteditable ref="contentEditable">
+        This is a post
       </div>
     </div>
   </div>
